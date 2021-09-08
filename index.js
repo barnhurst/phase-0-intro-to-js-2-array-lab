824ebe2e30d4a33550cd9f81afa69e088336ff03
Christopher <c.barnhurst@live.com>
2021-09-08 14:01:25 -0500

    commit tree 9ed1c0f141da5ed9ab7ebba3b69ba8b01226da87

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 // Write your solution here!
-const cats = ["Milo", "Otis", "Garfield"]:
- char cats[] ;
+const cats = ["Milo", "Otis", "Garfield"]
+// destructivelyAppendCat() 
+//     cats.push['Ralph']
+function destructivelyAppendCat(name){
+    return cats.push(name)
+ }
+
+ function destructivelyPrependCat(name){
+    return cats.unshift(name)
+ }
+ function destructivelyRemoveLastCat(name){
+    return cats.pop(name)
+ }
+ function destructivelyRemoveFirstCat(name){
+    return cats.shift(name)
+ }
+ function appendCat(name) {
+     
+    return [...cats, name]
+ }
+ function prependCat(name) {
+     
+    return [name, ...cats]
+ }
+ function removeLastCat() {
+     return cats.slice(0, 2)
+ }
+ function removeFirstCat() {
+    return cats.slice(1)
+}
